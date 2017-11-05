da5e1a52cd795b0fa2d3f3e52aa29699e4ef5cc8
yupouri <yhonda@mikilab.doshisha.ac.jp>
2017-11-05 15:53:42 +0900

    Lesson/Scss_with_Gulp/gulpfile.js内にコメントを追加

--- Scss_with_Gulp/gulpfile.js
+++ Scss_with_Gulp/gulpfile.js
@@ -6,7 +6,10 @@ gulp.task('default', function() {
 });
 
 gulp.task('sass', function() {
+		// src()：読みに行くファイルを指定
 		gulp.src('./scss/**/*.scss')
+		// sass()："gulp-sass"プラグインを使用
 				.pipe(sass())
+		// dest()：出力先の指定
 				.pipe(gulp.dest('./css'))		
 });
