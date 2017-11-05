6d4f29db5fd9d605665ea853dabd19a82a70d263
yupouri <yhonda@mikilab.doshisha.ac.jp>
2017-11-05 15:58:04 +0900

    Gulpの監視タスクを追加

--- Scss_with_Gulp/gulpfile.js
+++ Scss_with_Gulp/gulpfile.js
@@ -5,6 +5,7 @@ gulp.task('default', function() {
 		console.log("hoge")
 });
 
+// 自動コンパイルタスク
 gulp.task('sass', function() {
 		// src()：読みに行くファイルを指定
 		gulp.src('./scss/**/*.scss')
@@ -12,4 +13,10 @@ gulp.task('sass', function() {
 				.pipe(sass())
 		// dest()：出力先の指定
 				.pipe(gulp.dest('./css'))		
+});
+
+// ファイル更新有無の監視タスク
+gulp.task('watch', function() {
+		// 1つ目の[]内が監視するファイルのパス，2つ目の[]内は実行するGulpタスク
+		gulp.watch(['./scss/**/*'],['sass']);
 });
